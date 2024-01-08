fd82344d3e4aeca4861ac03f6c79c0fd1f71486e
vaibhavverulkar08 <vaibhavverulkar08@gmail.com>
2024-01-09 00:10:38 +0530

    demo code to launch webpage

---
+++ seleniumlearning/ChromeBrowserLaunchDemo.java
@@ -0,0 +1,25 @@
+package com.seleniumlearning;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowserLaunchDemo {
+	public static void main(String[] args) {
+
+		// Creating a driver object referencing WebDriver interface
+		WebDriver driver;
+
+		// Setting the webdriver.chrome.driver property to its executable's location
+		//System.setProperty("webdriver.chrome.driver", "C:/MyEclipseWorkspace/AmazonWebAppTesting/Drivers/chromedriver.exe");
+
+		// Instantiating driver object
+		driver = new ChromeDriver();
+
+		// Using get() method to open a web page
+		driver.get("https://www.amazon.in/");
+
+		// Closing the browser
+		driver.quit();
+
+	}
+}
